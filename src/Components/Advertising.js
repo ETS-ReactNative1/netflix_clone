f4e2dcc68490ea876f4275d3a2ad6c18ea0e9cca
Atul <atul22g2468@gmail.com>
2022-03-03 11:45:18 +0530

    Add advertising 2

--- src/Components/Advertising.js
+++ src/Components/Advertising.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Tv from "./images/tv.png";
+import Mobile from "./images/mobile.jpg";
 import "./Css/Advertising.css";
 
 export default function Advertising() {
   return (
     <section className="Advertising">
-    {/* first */}
+      {/* first */}
       <section className="first">
         <div className="container">
           <section className="text">
@@ -15,14 +16,22 @@ export default function Advertising() {
               Blu-ray players and more.
             </h2>
           </section>
-          <img src={Tv} alt="Tv" className="Tv"/>
+          <img src={Tv} alt="Tv" className="Tv" />
         </div>
       </section>
       {/* second */}
-      {/* <section className="second">
-        <div>s</div>
+      <section className="second">
+        <div className="container">
+          <section className="text">
+            <h1>Download your shows to watch offline.</h1>
+            <h2>
+              Save your favourites easily and always have something to watch.
+            </h2>
+          </section>
+          <img src={Mobile} alt="Tv" className="Mobile" />
+        </div>
       </section>
-      <section className="third">
+      {/* <section className="third">
         <div>d</div>
       </section>
       <section className="fourth">
